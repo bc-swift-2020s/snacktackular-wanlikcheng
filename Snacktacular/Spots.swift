d11862cd77807fc8c29854bc298d92c26b5ecb2d
kelvin <chengwj@bc.edu>
2020-04-11 18:11:54 -0700

    Completed - 9.2 UI tour + Creating the Spot and Spots classes

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,13 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Kelvin Cheng on 4/11/20.
+//  Copyright © 2020 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+class Spots {
+    var spotArray = [Spot]()
+}
