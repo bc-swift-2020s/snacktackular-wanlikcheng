7a9dd96df83b14099b7b710a67ce4db3c954d597
kelvin <chengwj@bc.edu>
2020-04-12 16:22:57 -0700

    Completed - 9.5 Implementing the Cloud Firestore Database and Saving Data—MVC-Style

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -34,6 +34,16 @@ class SpotDetailViewController: UIViewController {
         addressField.text = spot.address
     }
 
+    func leaveViewController()
+    {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
     @IBAction func photoButtonPressed(_ sender: UIButton) {
     }
     
@@ -41,15 +51,17 @@ class SpotDetailViewController: UIViewController {
     }
     
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        spot.saveData { success in
+            if success {
+                self.leaveViewController()
+            } else {
+                print("ERROR: Couldn't leave this view controller because data wasn't saved.")
+            }
+        }
     }
     
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
-        let isPresentingInAddMode = presentingViewController is UINavigationController
-        if isPresentingInAddMode {
-            dismiss(animated: true, completion: nil)
-        } else {
-            navigationController?.popViewController(animated: true)
-        }
+        leaveViewController()
     }
     
     @IBAction func lookupPlacePressed(_ sender: UIBarButtonItem) {
