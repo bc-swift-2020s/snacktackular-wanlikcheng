8ee0ea1d372298c38241145807f5914e6af58959
kelvin <chengwj@bc.edu>
2020-04-19 16:17:08 -0700

    Completed - 9.14 Creating the Review Class and Saving a Collection Inside a Document

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -24,6 +24,7 @@ class SpotDetailViewController: UIViewController {
     @IBOutlet weak var cancelBarButton: UIBarButtonItem!
     
     var spot: Spot!
+    var reviews: [Review] = []
     let regionDistance: CLLocationDistance = 750 // 750 meters
     var locationManager: CLLocationManager!
     var currentLocation: CLLocation!
@@ -63,6 +64,27 @@ class SpotDetailViewController: UIViewController {
         let region = MKCoordinateRegion(center: spot.coordinate, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
         mapView.setRegion(region, animated: true)
         updateUserInterface()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        spot.name = nameField.text!
+        spot.address = addressField.text!
+        switch segue.identifier ?? "" {
+        case "AddReview" :
+            let navigationController = segue.destination as! UINavigationController
+            let destination = navigationController.viewControllers.first as! ReviewTableViewController
+            destination.spot = spot
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                tableView.deselectRow(at: selectedIndexPath, animated: true)
+            }
+        case "ShowReview" :
+            let destination = segue.destination as! ReviewTableViewController
+            destination.spot = spot
+            let selectedIndexPath = tableView.indexPathForSelectedRow!
+            destination.review = reviews[selectedIndexPath.row]
+        default:
+            print("ERROR: Did not have a segue in SpotDetailViewController prepare(for segue:)")
+        }
     }
     
     func showAlert(title: String, message: String) {
